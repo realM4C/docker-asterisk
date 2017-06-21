a8565762e56814eac8550815633aeef999d9f34e
real_M4C <sag_i_dir_ned@gmx.at>
2017-06-21 18:31:16 +0200

    Create menuselect.sh

---
+++ data/menuselect.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+menuselect/menuselect \
+--enable-category MENUSELECT_ADDONS \
+--enable-category MENUSELECT_APPS \
+--enable-category MENUSELECT_CHANNELS \
+--enable-category MENUSELECT_CODECS \
+--enable-category MENUSELECT_FORMATS \
+--enable-category MENUSELECT_FUNCS \
+--enable-category MENUSELECT_RES \
+--enable codec_opus \
+--enable codec_g729a \
+--disable BUILD_NATIVE \
+--disable res_digium_phone \
+--disable codec_g729a \
+menuselect.makeopts
