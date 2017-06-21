ec0344f0356460fb8f52daed67ea7e877a0889e0
real_M4C <sag_i_dir_ned@gmx.at>
2017-06-21 15:44:24 +0200

    Create entry.sh

---
+++ data/entry.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+function update_ownership(){
+        chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /var/lib/asterisk
+        chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /var/log/asterisk
+        chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /var/run/asterisk
+        chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /var/spool/asterisk
+        chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /usr/lib/asterisk
+        chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /etc/asterisk
+        #chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /dev/zap
+        #chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /dev/dahdi
+        #chmod -R u=rwX,g=rX,o= /var/lib/asterisk
+        #chmod -R u=rwX,g=rX,o= /var/log/asterisk
+        #chmod -R u=rwX,g=rX,o= /var/run/asterisk
+        #chmod -R u=rwX,g=rX,o= /var/spool/asterisk
+        #chmod -R u=rwX,g=rX,o= /usr/lib/asterisk
+        #chmod -R u=rw,g=r,o= /etc/asterisk
+}
+echo -n "$(date): "
+update_ownership
+if [ "x${1}" = "x" ]; then
+        # This works if CMD is empty or not specified in Dockerfile
+        exec /usr/sbin/asterisk -vvvdddfcTin -U ${ASTERISK_USER} -G ${ASTERISK_GROUP}
+else
+        exec $@
+fi
