c0baa11c25e866025017e616dd1e0b06508b5360
real_M4C <sag_i_dir_ned@gmx.at>
2017-06-26 16:35:08 +0200

    Create hooks for FreePBX GUI and internal DB

--- data/entry.sh
+++ data/entry.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 function update_ownership(){
-	chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /var/lib/asterisk
-	chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /var/log/asterisk
-	chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /var/run/asterisk
-	chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /var/spool/asterisk
-	chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /usr/lib/asterisk
 	chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /etc/asterisk
+	chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /var/{lib,log,run,spool}/asterisk
+	chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /usr/lib/asterisk
 	#chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /dev/zap
 	#chown -R ${ASTERISK_USER}:${ASTERISK_GROUP} /dev/dahdi
 	#chmod -R u=rwX,g=rX,o= /var/lib/asterisk
@@ -16,11 +13,17 @@ function update_ownership(){
 	#chmod -R u=rw,g=r,o= /etc/asterisk
 }
 echo -n "$(date): "
-#update_ownership
+update_ownership
 if [ "x${1}" = "x" ]; then
 	# This works if CMD is empty or not specified in Dockerfile
-	#exec /usr/sbin/asterisk -vvvdddfcTin -U ${ASTERISK_USER} -G ${ASTERISK_GROUP}
-	exec /usr/sbin/asterisk -vvvdddfTin
+	if [ "x${ASTERISK_INST_GUI}" != "xno" ]; then
+		service apache2 start
+	fi
+	if [ "x${ASTERISK_INT_DB}" != "no" ]; then
+		service mysql start
+	fi
+	exec /usr/sbin/asterisk -vvvdddfTin -U ${ASTERISK_USER} -G ${ASTERISK_GROUP}
+	#exec /usr/sbin/asterisk -vvvdddfTin
 else
 	exec $@
 fi
